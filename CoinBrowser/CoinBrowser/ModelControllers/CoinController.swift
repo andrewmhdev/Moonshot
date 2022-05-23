93d3ed7e9c618619250d55b45a684a9fdc65805c
Andrew H <andrewdev@Andrews-MacBook-Air.local>
2022-05-23 17:10:49 -0400

    completed url networking

--- CoinBrowser/CoinBrowser/ModelControllers/CoinController.swift
+++ CoinBrowser/CoinBrowser/ModelControllers/CoinController.swift
@@ -27,6 +27,34 @@ class CoinController {
         let coinsUrl = baseUrl.appendingPathComponent(keyCoinsComponent)
         let finalUrl = coinsUrl.appendingPathComponent(keyListComponent)
         print(finalUrl)
+        URLSession.shared.dataTask(with: finalUrl) { coinData, _, error in
+            
+            if let error = error {
+                print("There was an \(error.localizedDescription)")
+                    completion(false)
+            }
+            guard let data = coinData else {completion(false); return}
+            
+            do{
+                if let topLevelArrayOfCoinDictionaries = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [[String:String]] {
+                    
+                    for coinDictionary in topLevelArrayOfCoinDictionaries {
+                        if let id = coinDictionary["id"],
+                           let symbol = coinDictionary["symbol"],
+                           let name = coinDictionary["name"] {
+                            let parsedCoin = Coin(id: id, symbol: symbol, name: name)
+                            coins.append(parsedCoin)
+                        }
+                    }
+                }
+                completion(true)
+            } catch {
+                print("error in do/try/catch\(error.localizedDescription)")
+                completion(false)
+            }
+
+            
+        } .resume()
     }
     
 }
